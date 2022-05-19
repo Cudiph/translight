4c1e33a67e3bdc6221756c6d190a282161a77dee
Cudiph <dwiaceromo@gmail.com>
2022-05-19 14:54:13 +0700

    change the way summon tooltip from click to mouseup event

--- src/content/content-script.ts
+++ src/content/content-script.ts
@@ -39,7 +39,7 @@ browser.runtime.onMessage.addListener(msg => {
   if (msg.name !== 'tabchanged') return;
 
   browser.storage.local.set({
-    selection: window.getSelection().toString(),
+    selection: getSelectionText(),
     windowURL: window.location.href,
   });
 });
@@ -59,37 +59,60 @@ function addTooltip(tooltipParent: HTMLElement, x: number, y: number) {
   const icon = document.createElement('img');
   icon.src = browser.runtime.getURL('images/translight-48.png');
   icon.alt = 'translate';
-  icon.classList.add('translight-icon');
+  icon.classList.add('translight-icon', 'translight-clickable');
   icon.style.position = 'absolute';
   icon.style.top = `${y}px`;
   icon.style.left = `${x}px`;
-  icon.style.animation = 'translight-fade .1s ease-in-out'
+  icon.style.animation = 'translight-fade .1s ease-in-out';
   tooltipParent.appendChild(icon);
   return icon;
 }
 
+const clamp = (num: number, min: number, max: number) => Math.min(Math.max(num, min), max);
+function getWidth() {
+  return Math.max(
+    document.body.scrollWidth,
+    document.documentElement.scrollWidth,
+    document.body.offsetWidth,
+    document.documentElement.offsetWidth,
+    document.documentElement.clientWidth
+  );
+}
+
+function getHeight() {
+  return Math.max(
+    document.body.scrollHeight,
+    document.documentElement.scrollHeight,
+    document.body.offsetHeight,
+    document.documentElement.offsetHeight,
+    document.documentElement.clientHeight
+  );
+}
 // event handler to handle whether inline popup should work as expected
 let container: HTMLDivElement;
 let tooltip: HTMLDivElement;
 let isTranslating = false;
-document.addEventListener("click", (e) => {
+document.addEventListener("mouseup", (e) => {
   const selected = getSelectionText();
 
   if (tooltip && !tooltip.contains(e.target as HTMLElement)) {
     tooltip.remove();
   }
   const clickingExtButton = (e.target as HTMLElement).classList.contains('translight-clickable');
-  
-  if (container && !container.contains(e.target as HTMLElement) && !isTranslating && !clickingExtButton) {
+
+  if (container && !container.contains(e.target as HTMLElement) && isTranslating && !clickingExtButton) {
     container.remove();
+    container = null;
   }
 
   if (!selected) return;
-  const x = e.pageX - 5;
-  const y = e.pageY + 12;
+  const x = clamp(e.pageX - 11, 22, getWidth() - 33);
+  const y = clamp(e.pageY + 15, 11, getHeight() - 44);
 
+  if (!container) isTranslating = false;
   if (!isTranslating && !clickingExtButton) tooltip = addTooltip(document.body, x, y);
 
+  // show result when the icon tooltip is clicked
   tooltip.onclick = async (_) => {
     isTranslating = true;
     tooltip.remove();
@@ -110,8 +133,6 @@ document.addEventListener("click", (e) => {
       }
     });
   };
-
-  isTranslating = false;
 });
 
 document.addEventListener('keydown', (e) => {
