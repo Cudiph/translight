4b4072342b2f116a5e8156b78b64712a1e000050
Cudiph <dwiaceromo@gmail.com>
2023-07-23 20:25:52 +0700

    sanitize examples response

--- src/popup/Result.svelte
+++ src/popup/Result.svelte
@@ -376,7 +376,7 @@
                   ><path d="M10,7L8,11H11V17H5V11L7,7H10M18,7L16,11H19V17H13V11L15,7H18Z" /></svg
                 >
               </td>
-              <td style="padding-bottom: 5px;">{@html example}</td>
+              <td style="padding-bottom: 5px;">{@html DOMPurify.sanitize(example)}</td>
             </tr>
           {/each}
         </table>
